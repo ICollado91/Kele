f65f390c66d275d2ec19f489c5804836190f6ed6
Ivan <collado.ip@gmail.com>
2017-06-16 03:08:29 +0000

    retrieve current user info

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,6 +4,14 @@ class Kele
     base_uri 'https://www.bloc.io/api/v1'
     
     def initialize(email, password)
-        @auth = self.class.post('/sessions', {email: email, password: password})
+        post_response = self.class.post("/sessions", body: {"email": email, "password": password})
+        raise "There was an error, check your input." if post_response.code != 200  
+        @auth_token = post_response["auth_token"]
     end
+    
+    def get_me
+        get_response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+        @user_data = JSON.parse(get_response.body) #user data is now a ruby hash!
+    end
+    
 end
